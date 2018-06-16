2ef9f037edbd20ae760c0d0493da3a6aa3a56af7
Joao Moraes <joaomoraesbr@gmail.com>
2018-06-16 00:20:25 -0300

    add interfaces

--- src/Interfaces/IAutoload.php
+++ src/Interfaces/IAutoload.php
@@ -2,8 +2,20 @@
 
 namespace FcPhp\Autoload\Interfaces
 {
+	use FcPhp\Autoload\Interfaces\IAutoload;
+	
 	interface IAutoload
 	{
-		
+		public static function getInstance() :IAutoload;
+
+		public function path(string $pathExpression, array $fileNameMatch, array $extensionMatch) :IAutoload;
+
+		public function get(string $key = null) :array;
+
+		public function beforeMatch($clousure) :IAutoload;
+
+		public function beforeMatchAgain($clousure) :IAutoload;
+
+		public function beforeStorage($clousure) :IAutoload;
 	}
 }
